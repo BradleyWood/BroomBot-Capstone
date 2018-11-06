f470f008382907bd6f8a8c3ccd526ba16ccf4827
BradleyWood <bradley.wood@uoit.net>
2018-11-05 21:29:01 -0500

    provision gpio pins as pwm

--- src/main/java/ca/uoit/crobot/Application.java
+++ src/main/java/ca/uoit/crobot/Application.java
@@ -7,19 +7,23 @@ public class Application {
     public static void main(String[] args) throws InterruptedException {
         final GpioController controller = GpioFactory.getInstance();
 
-        final GpioPinDigitalOutput leftPmw = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LEFT_PMW");
+        final GpioPinPwmOutput leftPmw = controller.provisionPwmOutputPin(RaspiPin.GPIO_01, "LEFT_PMW");
         final GpioPinDigitalOutput leftMotorDirection = controller.provisionDigitalOutputPin(RaspiPin.GPIO_04, "LEFT_DIR");
 
-        final GpioPinDigitalOutput rightPmw = controller.provisionDigitalOutputPin(RaspiPin.GPIO_26, "RIGHT_PMW");
+        final GpioPinPwmOutput rightPmw = controller.provisionPwmOutputPin(RaspiPin.GPIO_26, "RIGHT_PMW");
         final GpioPinDigitalOutput rightMotorDirection = controller.provisionDigitalOutputPin(RaspiPin.GPIO_27, "RIGHT_DIR");
 
         leftPmw.setShutdownOptions(true, PinState.LOW);
         rightPmw.setShutdownOptions(true, PinState.LOW);
 
+        leftMotorDirection.setShutdownOptions(true, PinState.LOW);
+        rightMotorDirection.setShutdownOptions(true, PinState.LOW);
+
+
         System.out.println("Test run for 10 seconds.");
 
-        leftPmw.setState(PinState.HIGH);
-        rightPmw.setState(PinState.HIGH);
+        leftPmw.setPwm(1024);
+        rightPmw.setPwm(1024);
 
         Thread.sleep(10000);
 
