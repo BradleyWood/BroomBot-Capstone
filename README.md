e66ce3207ec3a5ea0734cc4c4351c0ec6748748d
BradleyWood <bradley.wood@uoit.net>
2018-11-05 19:22:40 -0500

    basic application to move the wheels

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Cleaning Robot
+
+
+
+# Installation
+
+```
+mvnw install
+```
+
+# Execution
+
+```
+java -jar target/crobot-jar-with-dependencies.jar
+```
--- src/main/java/ca/uoit/crobot/Application.java
+++ src/main/java/ca/uoit/crobot/Application.java
@@ -1,7 +1,28 @@
 package ca.uoit.crobot;
 
+import com.pi4j.io.gpio.*;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        final GpioController controller = GpioFactory.getInstance();
+
+        final GpioPinDigitalOutput leftPmw = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LEFT_PMW");
+        final GpioPinDigitalOutput leftMotorDirection = controller.provisionDigitalOutputPin(RaspiPin.GPIO_04, "LEFT_DIR");
+
+        final GpioPinDigitalOutput rightPmw = controller.provisionDigitalOutputPin(RaspiPin.GPIO_26, "RIGHT_PMW");
+        final GpioPinDigitalOutput rightMotorDirection = controller.provisionDigitalOutputPin(RaspiPin.GPIO_27, "RIGHT_DIR");
+
+        leftPmw.setShutdownOptions(true, PinState.LOW);
+        rightPmw.setShutdownOptions(true, PinState.LOW);
+
+        System.out.println("Test run for 10 seconds.");
+
+        leftPmw.setState(PinState.HIGH);
+        rightPmw.setState(PinState.HIGH);
+
+        Thread.sleep(10000);
+
+        controller.shutdown();
     }
 }
