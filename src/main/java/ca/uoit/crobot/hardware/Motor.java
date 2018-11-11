ab466be5488f0f24beeae4bfc334c0bd501cea2f
BradleyWood <bradley.wood@uoit.net>
2018-11-10 22:14:39 -0500

    pwm motor controller with optional enable pin

--- src/main/java/ca/uoit/crobot/hardware/Motor.java
+++ src/main/java/ca/uoit/crobot/hardware/Motor.java
@@ -3,19 +3,27 @@ package ca.uoit.crobot.hardware;
 public interface Motor {
 
     /**
-     * Set the speed of the device, as a percent
+     * Set the speed of the device, as a percent.
+     * The sign denotes the direction of the motor.
      *
-     * @param speed The speed out of 100
+     * @param speed The speed [-100, 100]
      */
     void setSpeed(int speed);
 
     /**
-     * The current speed of the motor, out of 100
+     * The current speed of the motor [-100, 100]
+     * The sign denotes the direction of the motor.
      */
-    void getSpeed();
+    int getSpeed();
 
     /**
      * Stops the motor from turning
      */
     void stop();
+
+    /**
+     * Perform initial setup of the device
+     */
+    void init();
+
 }
