894fc1a7e8a3da324728d2553658f99cae693038
BradleyWood <bradley.wood@uoit.net>
2018-12-23 15:16:38 -0500

    implement simulated motor

---
+++ simulator/src/main/java/ca/uoit/crobot/hardware/SimulatedMotor.java
@@ -0,0 +1,50 @@
+package ca.uoit.crobot.hardware;
+
+import ca.uoit.crobot.model.GameObject;
+import lombok.Data;
+
+public @Data class SimulatedMotor implements Motor {
+
+    private final GameObject robot;
+    private SimulatedMotor otherMotor;
+
+    private int counter;
+    private int speed;
+
+    @Override
+    public void setSpeed(final int speed) {
+        this.speed = speed;
+
+
+        if (otherMotor != null) {
+            final int otherSpeed = otherMotor.getSpeed();
+            final int diff = speed - otherSpeed;
+
+            robot.setSpeed((speed + otherSpeed) / 16f);
+            robot.setAngularVelocity(diff / 800f);
+        } else {
+            throw new NullPointerException("Cannot calculate simulated velocity without reference to" +
+                    " both robot motors.");
+        }
+    }
+
+    @Override
+    public int getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void stop() {
+        setSpeed(0);
+    }
+
+    @Override
+    public int getCount() {
+        return counter;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
