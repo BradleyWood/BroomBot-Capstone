987b8258e9dc59df0b9de7a33014ba07c8b51a07
BradleyWood <bradley.wood@uoit.net>
2019-01-18 14:52:30 -0500

    fix build by deleting unused class

--- android-app/src/main/java/ca/uoit/crobot/MainActivity.java
+++ android-app/src/main/java/ca/uoit/crobot/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
