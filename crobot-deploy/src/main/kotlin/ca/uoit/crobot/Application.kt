ad1ff8cb79179c85f61f4d47334cf9e0fad0c22d
BradleyWood <bradley.wood@uoit.net>
2019-01-31 19:36:51 -0500

    deploy app to cloud for update feature

--- crobot-deploy/src/main/kotlin/ca/uoit/crobot/Application.kt
+++ crobot-deploy/src/main/kotlin/ca/uoit/crobot/Application.kt
@@ -11,12 +11,22 @@ import io.ktor.gson.GsonConverter
 import io.ktor.gson.gson
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.PartData
+import io.ktor.http.content.forEachPart
+import io.ktor.http.content.streamProvider
+import io.ktor.network.util.ioCoroutineDispatcher
+import io.ktor.request.receiveMultipart
 import io.ktor.response.respond
 import io.ktor.response.respondFile
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
 import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 
 fun Application.main() {
     install(DefaultHeaders)
@@ -57,7 +67,43 @@ fun Application.main() {
         }
 
         post("/deploy") {
+            val version = call.parameters["version"]
+            val multipart = call.receiveMultipart()
+            multipart.forEachPart { part ->
+                when (part) {
+                    is PartData.FileItem -> {
+                        val file = File(appFiles, "crobot-app-$version.jar")
+                        part.streamProvider().use { input -> file.outputStream().buffered().use { output -> input.copyToSuspend(output) } }
+                    }
+                }
 
+                part.dispose()
+            }
+            versionFile.appendText(System.lineSeparator() + version)
         }
+    }
+}
+
+suspend fun InputStream.copyToSuspend(
+        out: OutputStream,
+        bufferSize: Int = DEFAULT_BUFFER_SIZE,
+        yieldSize: Int = 4 * 1024 * 1024,
+        dispatcher: CoroutineDispatcher = ioCoroutineDispatcher
+): Long {
+    return withContext(dispatcher) {
+        val buffer = ByteArray(bufferSize)
+        var bytesCopied = 0L
+        var bytesAfterYield = 0L
+        while (true) {
+            val bytes = read(buffer).takeIf { it >= 0 } ?: break
+            out.write(buffer, 0, bytes)
+            if (bytesAfterYield >= yieldSize) {
+                yield()
+                bytesAfterYield %= yieldSize
+            }
+            bytesCopied += bytes
+            bytesAfterYield += bytes
+        }
+        return@withContext bytesCopied
     }
 }
